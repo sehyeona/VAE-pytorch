9cfc25647e3f471e427f99e4718f14d5e96b210a
Ubuntu <ubuntu@ip-172-31-52-219.ec2.internal>
2020-09-27 14:04:03 +0000

    fix: temp vector=mu

--- core/utils.py
+++ core/utils.py
@@ -5,9 +5,18 @@ import torch
 
 cuda = True if torch.cuda.is_available() else False
 Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+print(cuda)
 
-def reparameterization(latent_dim, mu, logvar, ):
+def reparameterization(latent_dim, mu, logvar):
     std = torch.exp(logvar / 2)
-    sampled_z = Variable(Tensor(np.random.normal(0, 1, (mu.size(0), latent_dim))))
+    sampled_z = 0
+    try:
+        sampled_z = Variable(Tensor(np.random.normal(0, 1, (mu.size(0), latent_dim))))
+    except: 
+        sampled_z = Variable(torch.FloatTensor(np.random.normal(0, 1, (mu.size(0), latent_dim))))
     z = sampled_z * std + mu
     return z
+
+def tensor2ndarray255(images):
+    images = torch.clamp(images * 0.5 + 0.5, 0, 1)
+    return images.cpu().numpy().transpose(0, 2, 3, 1) * 255
